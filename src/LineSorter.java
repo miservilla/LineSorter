dcbf09550965c0ebfbad970dcd6df6980bfd4ffe
miservilla <miservilla@gmail.com>
2017-03-04 19:28:43 -0700

    Final with JavaDocs.

--- src/LineSorter.java
+++ src/LineSorter.java
@@ -6,28 +6,44 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 /**
+ * A CLI class to read in a text file, sort it based on length of lines, and
+ * lexicographically if length of the two are equal. It will then write the
+ * sorted results to a new file.
  * @author Michael Servilla
  * @version date 2017-02-28
  */
 public class LineSorter {
+    /**
+     * Main method in the class to run program.
+     * @param args The first string argument is the name of the text file to
+     *             read, and the second is the file to be written to.
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        Path source = Paths.get("testinput.txt"); //for source test file
-        Path target = Paths.get("testoutput.txt"); //for target test file
-        ArrayList<String> lines = new ArrayList<>();
+        Path source = Paths.get(args[0]); //For source text file.
+        Path target = Paths.get(args[1]); //For target text file
+        ArrayList<String> lines = new ArrayList<>(); //New array list to store
+        //and sort file.
+        /**
+         * Try/Catch block using resources for reading in text file.
+         */
         try (BufferedReader reader = Files.newBufferedReader(source))
         {
-            String line = null;
+            String line;
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+        //Sort methods, the second using Lambda expressions.
         Collections.sort(lines);
-        lines.sort((s1, s2) -> (s1.length() - "line".length()) - (s2.length() -
-                "line".length()));
-        //http://stackoverflow.com/questions/7575761/
-        // sort-arraylist-of-strings-by-length
+        lines.sort((s1, s2) -> s1.length() - s2.length());
+
+        /**
+         * Try/Catch block using resources for writing sorted text file.
+         */
         try (BufferedWriter writer = Files.newBufferedWriter(target))
         {
             Iterator<String> iterator = lines.iterator();
